6042b529ee4b4f9e28950c8b4f062bb85058d5b3
Michael Carroll <mjoecarroll@gmail.com>
2025-09-12 13:53:45 -0400

    Add power button

--- src/sections/music-player.ts
+++ src/sections/music-player.ts
@@ -9,6 +9,7 @@ import styles from '../styles/music-player';
 import { 
   mdiPause, 
   mdiPlay, 
+  mdiPower, 
   mdiRepeat, 
   mdiRepeatOff, 
   mdiRepeatOnce, 
@@ -146,6 +147,9 @@ class MusicPlayerCard extends LitElement {
     this.player_data.repeat = repeat;
     await this.actions.actionRepeatSet(this._player, repeat);
     
+  }
+  private onToggle = async () => {
+    await this.actions.actionTogglePlayer(this._player);
   }
   private toTime(seconds: number) {
     if (isNaN(seconds)) {
@@ -381,6 +385,18 @@ class MusicPlayerCard extends LitElement {
   }
   protected renderVolume() {
     return html`
+      <ha-button
+        appearance="plain"
+        variant="brand"
+        size="medium"
+        class="button-power"
+        @click=${this.onToggle}
+      >
+        <ha-svg-icon
+          .path=${mdiPower}
+          style="height: 3rem; width: 3rem;"
+        ></ha-svg-icon>
+      </ha-button>
       <ha-control-slider
         .disabled=${this.player_data.muted}
         .unit="%"
--- src/styles/music-player.ts
+++ src/styles/music-player.ts
@@ -86,6 +86,9 @@ export default css`
     padding-left: 2em;
     padding-right: 2em;
   }
+  .button-power {
+    margin-right: 1em;
+  }
   ha-control-slider {
     --control-slider-thickness: 1.75em;
   }
