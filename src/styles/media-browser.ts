b170405a4a7a4d38103b85460854a2ee75b14dfe
Michael Carroll <mjoecarroll@gmail.com>
2025-09-18 15:36:08 -0400

    Wrap browser in ha-card

--- src/styles/media-browser.ts
+++ src/styles/media-browser.ts
@@ -3,8 +3,8 @@ import { css } from 'lit';
 // Styles belonging to the card
 // https://lit.dev/docs/components/styles/
 export default css`
-  .mass-browser {
-    display: flex;
+  ha-card {
+    box-shadow: unset;
     height: var(--mass-player-card-height);
   }
 .header {
