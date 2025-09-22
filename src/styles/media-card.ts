68274b44dbb93059a7f508adae27c9b65319a049
Michael Carroll <mjoecarroll@gmail.com>
2025-09-22 14:48:28 -0400

    Create single media card element

--- src/styles/media-card.ts
+++ src/styles/media-card.ts
@@ -3,23 +3,10 @@ import { css } from 'lit';
 // Styles belonging to the card
 // https://lit.dev/docs/components/styles/
 export default css`
-  ha-control-button {
-    --section-thumbnail-size: calc(var(--mass-player-card-height) / 5);
-    --thumbnail-size: calc(var(--mass-player-card-height) / 2.5 - 2em);
-    width: 98%;
-    height: 98%;
-    margin: 1%;
-  }
-  .thumbnail {
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    margin: 0 6%;
-    height: var(--thumbnail-size);
-    width: var(--thumbnail-size);
-    padding-left: 1em;
-    padding-right: 1em;
-    padding-bottom: 2em;
+  ha-card {
+    box-shadow: unset;
+    display: flex;
+    position: relative;
   }
   .thumbnail-section {
     background-repeat: no-repeat;
@@ -27,7 +14,32 @@ export default css`
     height: var(--section-thumbnail-size);
     width: var(--section-thumbnail-size);
   }
-  .title {
+  ha-control-button {
+    --section-thumbnail-size: calc(var(--mass-player-card-height) / 5);
+    --thumbnail-size: calc(var(--mass-player-card-height) / 2.5 - 2em);
+    width: 98%;
+    height: 98%;
+    margin: 1%;
+  }
+  #container {
+    width: 100%;
+    height: calc(var(--mass-player-card-height) / 2);
+    position: relative;
+  }
+
+  #thumbnail-div {
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin: 0 6%;
+    height: var(--thumbnail-size);
+    width: var(--thumbnail-size);
+    padding-bottom: 2em;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+
+  #title-div {
     font-size: 1.2rem;
     text-transform: capitalize;
     position: absolute;
@@ -35,5 +47,53 @@ export default css`
     line-height: 160%;
     bottom: 0;
     background-color: rgba(from var(--ha-card-background) r g b / 0.9);
+  }
+
+  #enqueue-menu-control {
+    --control-select-menu-background-color: unset;
+    --ha-ripple-color: rgba(0,0,0,0);
+    --mdc-icon-size: 5em;
+    --control-select-menu-height: 6em;
+  }
+  #card-button-div {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: contents;
+    top: 0;
+    left: 0;
+  }
+  mass-menu-button::part(menu-button) {
+    position: absolute;
+    bottom: 1.5em;
+    right: -0.75em;
+    --ha-ripple-color: rgba(0,0,0,0);
+  }
+  mass-menu-button::part(menu-select-menu) {
+    --mdc-icon-size: 5em;
+    --control-select-menu-height: 6em;
+    --control-select-menu-background-color: unset;
+  }
+  mass-menu-button::part(menu-svg) {
+    color: var(--mdc-theme-primary);
+    background-color: var(--ha-card-background,var(--card-background-color,#fff));
+    border-radius: 50%;
+  }
+  mass-menu-button::part(menu-list-item) {
+  }
+  mass-menu-button::part(menu-list-item-svg) {
+    height: 2em;
+    width: 2em;
+  }
+  #enqueue-list-item {
+  }
+  .enqueue-item-svg {
+    height: 2em;
+    width: 2em;
+  }
+  #enqueue-svg {
+    color: var(--mdc-theme-primary);
+    background-color: var(--ha-card-background,var(--card-background-color,#fff));
+    border-radius: 50%;
   }
 `;
