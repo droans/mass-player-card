d59de5a57a7a16c2507bcea16992db1747d4d756
Michael Carroll <mjoecarroll@gmail.com>
2025-10-08 15:30:44 -0400

    Utility functions

--- src/utils/music-player.ts
+++ src/utils/music-player.ts
@@ -1,5 +1,7 @@
 import { html } from "lit";
 import { PlayerIcon, PlayerIconSize } from "../config/player";
+import { RepeatMode } from "../const/common.js";
+import { mdiRepeat, mdiRepeatOff, mdiRepeatOnce } from "@mdi/js";
 
 export function generateControlSlotHtml(icon_style: PlayerIcon) {
   if (
@@ -19,4 +21,23 @@ export function generateControlLabelHtml(icon_style: PlayerIcon, label: string) 
     return html``;
   }
   return html`${label}`;
+}
+
+export function getIteratedRepeatMode(cur_repeat: RepeatMode): RepeatMode {
+  if (cur_repeat == RepeatMode.ALL) {
+    return RepeatMode.ONCE;
+  }
+  if (cur_repeat == RepeatMode.ONCE) {
+    return RepeatMode.OFF;
+  }
+  return RepeatMode.ALL;
+}
+export function getRepeatIcon(repeat: RepeatMode): string {
+  if (repeat == RepeatMode.ALL) {
+    return mdiRepeat;
+  }
+  if (repeat == RepeatMode.ONCE) {
+    return mdiRepeatOnce;
+  }
+  return mdiRepeatOff;
 }
