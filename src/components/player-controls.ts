01f9a6bbb8574d2555c7eeede26fc6e7f7199d0d
Michael Carroll <mjoecarroll@gmail.com>
2025-10-16 16:46:13 -0400

    Increase standardization

--- src/components/player-controls.ts
+++ src/components/player-controls.ts
@@ -1,38 +1,16 @@
-import { CSSResultGroup, LitElement, TemplateResult } from "lit";
-import styles from '../styles/player-controls';
-import { PlayerConfig, PlayerControlsLayout, PlayerIconSize, PlayerLayoutConfig } from "../config/player";
-import { ActionsController } from "../controller/actions";
 import { consume } from "@lit/context";
-import {
-  actionsControllerContext,
-  activePlayerDataContext,
-  IconsContext,
-  musicPlayerConfigContext
-} from "../const/context";
-import { PlayerData } from "../const/music-player";
-import { RepeatMode } from "../const/common";
+import { CSSResultGroup, html, TemplateResult } from "lit";
 import { state } from "lit/decorators.js";
-import {
-  html
-} from "lit/static-html.js";
-import {
-  generateControlLabelHtml,
-  generateControlSlotHtml,
-  getIteratedRepeatMode,
-  getRepeatIcon
-} from "../utils/music-player";
-import { Icons } from "../const/icons.js";
+import { MassPlayerControlsBase } from "./player-controls-base";
+import { PlayerConfig, PlayerControlsLayout, PlayerIconSize, PlayerLayoutConfig } from "../config/player";
+import { activePlayerDataContext, musicPlayerConfigContext } from "../const/context";
+import { generateControlLabelHtml, generateControlSlotHtml, getRepeatIcon } from "../utils/music-player";
+import { RepeatMode } from "../const/common";
+import styles from '../styles/player-controls';
 
-class MassPlayerControls extends LitElement {
-  @consume({ context: actionsControllerContext, subscribe: true})
-  private controller!: ActionsController;
-  @consume({ context: activePlayerDataContext, subscribe: true})
-  @state()
-  private player_data!: PlayerData;
-  @consume({ context: IconsContext}) private Icons!: Icons;
-  private _config!: PlayerConfig;
+class MassPlayerControls extends MassPlayerControlsBase {
   private layoutConfig!: PlayerLayoutConfig;
-  
+  private _config!: PlayerConfig;
   @consume({ context: musicPlayerConfigContext, subscribe: true})
   @state()
   private set config(config: PlayerConfig) {
@@ -45,32 +23,13 @@ class MassPlayerControls extends LitElement {
     this.layoutConfig = config.layout;
   }
 
-  private onPrevious = async () => {
-    await this.controller.actionPlayPrevious();
-  }
-  private onShuffle = async () => {
-    await this.controller.actionToggleShuffle();
-  }
-  private onPlayPause = async () => {
-    await this.controller.actionPlayPause();
-  }
-  private onNext = async () => {
-    await this.controller.actionPlayNext();
-  }
-  private onRepeat = async () => {
-    const cur_repeat = this.player_data.repeat;
-    const repeat = getIteratedRepeatMode(cur_repeat);
-    this.requestUpdate();
-    await this.controller.actionSetRepeat(repeat);
-  }
-
   protected renderShuffle(): TemplateResult {
     const icon_style = this.layoutConfig.icons.shuffle;
     const icon_size = icon_style.size == PlayerIconSize.LARGE ? "medium" : "small";
     const slotHtml = generateControlSlotHtml(icon_style);
     const labelHtml = generateControlLabelHtml(icon_style, 'Shuffle');
     const div_layout= this.layoutConfig.controls_layout == PlayerControlsLayout.COMPACT ? "div-compact" : "div-spaced";
-    const appearance = this.player_data.shuffle ? "accent" : "plain";
+    const appearance = this._playerData.shuffle ? "accent" : "plain";
     return html`
       <div class="shuffle div-${icon_size} ${div_layout}">
         <ha-button
@@ -82,7 +41,7 @@ class MassPlayerControls extends LitElement {
         >
           <ha-svg-icon
             ${slotHtml}
-            .path=${this.player_data.shuffle ? this.Icons.SHUFFLE : this.Icons.SHUFFLE_DISABLED}
+            .path=${this._playerData.shuffle ? this.Icons.SHUFFLE : this.Icons.SHUFFLE_DISABLED}
             class="svg-${icon_size}"
           ></ha-svg-icon>
           ${labelHtml}
@@ -119,9 +78,9 @@ class MassPlayerControls extends LitElement {
     const icon_size = icon_style.size == PlayerIconSize.LARGE ? "medium" : "small";
     const div_layout= this.layoutConfig.controls_layout == PlayerControlsLayout.COMPACT ? "div-compact" : "div-spaced";
     const slotHtml = generateControlSlotHtml(icon_style);
-    const label = this.player_data.playing ? 'Playing' : 'Paused';
+    const label = this._playerData.playing ? 'Playing' : 'Paused';
     const labelHtml = generateControlLabelHtml(icon_style, label);
-    const appearance = this.player_data.playing ? "filled" : "outlined";
+    const appearance = this._playerData.playing ? "filled" : "outlined";
     return html`
       <div class="play-pause div-${icon_size} ${div_layout}">
         <ha-button
@@ -133,7 +92,7 @@ class MassPlayerControls extends LitElement {
         >
           <ha-svg-icon
             ${slotHtml}
-            .path=${this.player_data.playing ?  this.Icons.PAUSE : this.Icons.PLAY}
+            .path=${this._playerData.playing ?  this.Icons.PAUSE : this.Icons.PLAY}
             class="svg-${icon_size} icon-outlined"
           ></ha-svg-icon>
           ${labelHtml}
@@ -168,13 +127,13 @@ class MassPlayerControls extends LitElement {
     `
   }
   protected renderRepeat(): TemplateResult {
-    const icon = getRepeatIcon(this.player_data.repeat, this.Icons);
+    const icon = getRepeatIcon(this._playerData.repeat, this.Icons);
     const div_layout= this.layoutConfig.controls_layout == PlayerControlsLayout.COMPACT ? "div-compact" : "div-spaced";
     const icon_style = this.layoutConfig.icons.repeat;
     const icon_size = icon_style.size == PlayerIconSize.LARGE ? "medium" : "small";
     const slotHtml = generateControlSlotHtml(icon_style);
     const labelHtml = generateControlLabelHtml(icon_style, 'Repeat');
-    const appearance = this.player_data.repeat == RepeatMode.OFF ? "accent" : "plain";
+    const appearance = this._playerData.repeat == RepeatMode.OFF ? "accent" : "plain";
     return html`
       <div class="repeat div-${icon_size} ${div_layout}">
         <ha-button
