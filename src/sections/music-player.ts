9165cfb3dee527a87cebc3fa1fbf429716533f10
Michael Carroll <mjoecarroll@gmail.com>
2025-10-29 14:11:57 -0400

    Update player data on reconnect

--- src/sections/music-player.ts
+++ src/sections/music-player.ts
@@ -87,7 +87,7 @@ class MusicPlayerCard extends LitElement {
 
   @provide({ context: activePlayerDataContext})
   @state()
-  private player_data!: PlayerData;
+  public player_data!: PlayerData;
 
   
   @state()
@@ -612,7 +612,11 @@ class MusicPlayerCard extends LitElement {
   }
   connectedCallback(): void {
     super.connectedCallback();
-    if (this._animation && this._firstLoaded) {
+    if (!this._firstLoaded) {
+      return;
+    }
+    this.updatePlayerData();
+    if (this._animation) {
       this._animation.play = true;
     }
   }
