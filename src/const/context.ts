1d1f4d9907ae43ad0b010233d02d60240c9de9ca
Michael Carroll <mjoecarroll@gmail.com>
2025-12-29 20:43:27 -0500

    Export UUID function

--- src/const/context.ts
+++ src/const/context.ts
@@ -65,7 +65,7 @@ export const mediaBrowserCardsContext =
 export const activeMediaBrowserCardsContext =
   createContext<MediaCardItem[]>(uuid4());
 
-function uuid4() {
+export function uuid4() {
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
     (
       +c ^
