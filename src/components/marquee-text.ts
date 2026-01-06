303a587fbb5938faafbed6da0c83220f83a3ac71
Michael Carroll <mjoecarroll@gmail.com>
2026-01-06 11:39:45 -0500

    Linting

--- src/components/marquee-text.ts
+++ src/components/marquee-text.ts
@@ -1,3 +1,3 @@
-import { MarqueeText } from "ha-components/packages/components/marquee-text.js";
+import { MarqueeText } from "@droans/ha-components/packages/components/marquee-text.js";
 
 customElements.define('mpc-marquee-text', MarqueeText);
