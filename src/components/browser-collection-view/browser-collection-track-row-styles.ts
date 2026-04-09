9540d572c2760585eea2d99c5b55ee3f06473ae4
Michael Carroll <mjoecarroll@gmail.com>
2026-04-09 09:02:12 -0400

    Set border radius on collection track row

--- src/components/browser-collection-view/browser-collection-track-row-styles.ts
+++ src/components/browser-collection-view/browser-collection-track-row-styles.ts
@@ -5,6 +5,7 @@ export default css`
     --md-list-item-top-space: 6px;
     --md-list-item-bottom-space: 6px;
     --md-list-item-trailing-space: 0px;
+    border-radius: var(--default-border-radius);
   }
   mass-menu-button::part(menu-select-menu) {
     --control-select-menu-background-opacity: 0;
