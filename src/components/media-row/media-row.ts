b70c3331968c976dd934bf5e4bdd9c0b05bfabb2
Michael Carroll <mjoecarroll@gmail.com>
2026-07-13 10:13:07 -0400

    Cache media rows

--- src/components/media-row/media-row.ts
+++ src/components/media-row/media-row.ts
@@ -39,6 +39,8 @@ import {
 import { Icons } from "../../const/icons";
 import { queueItem } from "mass-queue-types/packages/mass_queue/actions/get_queue_items";
 import { Config } from "../../config/config";
+import { cache } from "lit/directives/cache.js";
+import { DirectiveResult } from "lit/async-directive.js";
 
 @customElement("mpc-queue-media-row")
 export class MediaRow extends LitElement {
@@ -403,11 +405,11 @@ export class MediaRow extends LitElement {
     `;
   }
 
-  render(): TemplateResult {
+  render(): DirectiveResult {
     const playing = this.media_item?.playing ? `active` : ``;
     const expressive = this.useExpressive ? `expressive` : ``;
     const hideCovers = this.showAlbumCovers ? `` : `hide-covers`;
-    return html`
+    return cache(html`
       <ha-md-list-item
         style="${this.display ? "" : "display: none;"}"
         class="button ${playing} ${expressive} ${hideCovers}"
@@ -421,7 +423,7 @@ export class MediaRow extends LitElement {
         ${this.renderActionButtons()}
       </ha-md-list-item>
       <div class="divider"></div>
-    `;
+    `);
   }
   static get styles(): CSSResultGroup {
     return styles;
